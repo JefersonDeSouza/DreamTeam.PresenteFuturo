c129c537969a2f630776580fb621befd6bdec27e
Jeferson de Souza <je_souza7@hotmail.com>
2018-09-09 03:07:00 -0300

    Restante dos arquivos da primeira tela

---
+++ Site/ViewsModels/MensagemVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Site.ViewsModels
+{
+    public class MensagemVM
+    {
+        public string CssClassName { get; set; }
+        public string Titulo { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
